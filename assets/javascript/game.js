d1ea2c638b9b041838e8cd707d3477db06ed1e45
Corey Decato <Corey1@Coreys-MBP.lan>
2018-04-04 14:18:44 -0400

    basic html and java commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -53,114 +53,88 @@ $(document).ready(function() {
     };
 
     var wins = 0;
+    var losses = 0;
     var guesses = 10;
-    var wordIndex = 0;
     var guessedLetters = [];
-    var word = [];
-    var solved = false;
+    var displayWord = [];
+    var wordIndex = 0;
+    var wordPicked = "";
+    var playerGuess = "";
     var gameStarted = false;
-    var gameOver = false;
     var wordArray = [words.w1, words.w2, words.w3, words.w4, words.w5, words.w6, words.w7, words.w8, words.w9, words.w10, words.w11, words.w12,words.w13, words.w14, words.w15, words.w16, words.w17, words.w18, words.w19, words.w20, words.w21, words.w22, words.w23, words.w24, words.w25, words.w26, words.w27, words.w28, words.w29, words.w30, words.w31, words.w32, words.w33, words.w34, words.w35, words.w36, words.w37, words.w38, words.w39, words.w40, words.w41, words.w42, words.w43, words.w44, words.w45, words.w46, words.w47, words.w48, words.w49, words.w50];
 
-    function initializeGame() {
-        guesses = 10;
-        guessedLetters = [];
-        word = [];
-        solved = false;
-        gameStarted = true;
-        gameOver = false;
-        console.log(wordIndex);
-    }
+    pickWord();
 
-    function gameOn() {
-        if (guesses > 0) {
-            if (guessedLetters.indexOf(letter) === -1) {
-                guessedLetters.push(letter);
-                evaluateGuess(letter);
-                console.log(word);
+    function pickWord() {
+        if ( gameStarted == false) {
+            wordIndex = Math.floor(Math.random() * wordArray.length);
+            wordPicked = wordArray[wordIndex];
+            for (var i = 0; i < wordPicked.length; i++) {
+                if ( wordPicked[i] == " ") {
+                    displayWord[i] = "   ";
+                }
+                else {
+                    displayWord[i] = "_ ";
+                }
+                $("#currentWord").html("Current Word: " + displayWord.join(""));
             }
-
         }
+        gameStarted == true;
     }
 
-    function makeGuess(letter) {
-        if (guesses > 0) {
-            // if (!gameStarted) {
-            //     gameStarted == true;
-            // }
-            if (guessedLetters.indexOf(letter) === -1) {
-                guessedLetters.push(letter);
-                evaluateGuess(letter);
-                console.log(guessedLetters);
-            }
-            for ( i = 0; i < word.length; i++) {
-                if (letter = word[i]) {
-                    
+     function evaluateGuess() {
+            var positions = [];
+            for (var i = 0; i < wordPicked.length; i++) {
+                if (wordPicked[i] === playerGuess) {
+                    positions.push(i);
+                    displayWord[i] = playerGuess;
+                    $("#currentWord").html("Current Word: " + displayWord.join(""));
                 }
             }
+            if (positions.length <= 0) {
+                guesses--;
+                $("#guessesRemaining").html("Guesses Remaining: " + guesses);
+            }
         }
-        updateDisplay();
-        checkWin();
-    }
-
-    function updateDisplay() {
-        $("#wins").html("Wins: " + wins);
-        $("#guessesRemaining").html("Guesses Remaining: " + guesses);
-        $("#lettersGuessed").html("Letters Guessed: " + guessedLetters);
-        wordIndex = Math.floor(Math.random() * wordArray.length);
-        for (var i = 0; i < wordArray[wordIndex].length; i++) {
-            word.push("_ ");
-            $("#currentWord").html("Current Word: " + word.join(""));
-        }
-        // for (var j = 0; j < wordArray[wordIndex].length; j++) {
-        //     $("#currentWord").html("Current Word: " + word.join(""));
-        // }
-        if (guesses <= 0) {
-            gameOver = true;
-        }
-    }
 
     function checkWin() {
-        if (word.indexOf("_ ") === -1) {
+        if (displayWord.indexOf("_ ") === -1) {
             wins++;
-            solved = true;
+            $("#wins").html("Wins: " + wins);
+            resetGame();
+        }
+        if ( guesses == 0 ) {
+            losses++;
+            $("#losses").html("Losses: " + losses);
+            resetGame();
         }
     }
 
-    function evaluateGuess(letter) {
-        var positions = [];
-        for (var i = 0; i < wordArray[wordIndex].length; i++) {
-            if (wordArray[wordIndex][i] === letter) {
-                positions.push(i); 
-            }
-        }
-        if (positions.length <= 0) {
-            guesses--;
-        }
-        else {
-            for (var j = 0; j < positions.length; j++) {
-                word[positions[i]] = letter;
-            }
-        }
+    function resetGame() {
+        guesses = 10;
+        $("#guessesRemaining").html("Guesses Remaining: " + guesses);
+        guessedLetters = [];
+        $("#lettersGuessed").html("Letters Guessed: " + guessedLetters);
+        displayWord = [];
+        $("#currentWord").html("Current Word: " + displayWord.join(""));
+        gameStarted = false;
+        pickWord();
     }
 
-    $(document).keyup(function(event) {
-        var key = event.keyCode;
-        var letter = String.fromCharCode(key);
-        var letter = letter.toLowerCase();
-        if (solved == true) {
-            initializeGame();
-        }
-        else if (gameOver == true) {
-            initializeGame();
-        }
-        else if (gameStarted == true) {
-            gameOn();
-        }
-        else {
-            if (letter >= "a" && letter <= "z") {
-                makeGuess(letter.toLowerCase());
+    $(document).keyup( function(event) {
+        $("#alert").html("");
+        var letter = event.which;
+        playerGuess = String.fromCharCode(letter).toLowerCase();
+        if ( playerGuess >= "a" && playerGuess <= "z" ) {
+            if (guessedLetters.indexOf(playerGuess) === -1) {
+                guessedLetters.push(playerGuess);
+                $("#lettersGuessed").html("Letters Guessed: " + guessedLetters);
+                evaluateGuess(); 
+                checkWin();
+            }
+            else {
+                $("#alert").html("You've already guessed that letter!");
             }
         }
-    });  
+    }) 
 })
